ff2463b4b50e1c6c354badb3feebdea24eb954bd
AprilRoseNatad <aprilrosenatad11@gmail.com>
2026-03-03 12:07:46 +0800

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,15 +27,94 @@ animate(chars2, {
 });
 
 /*=============== SWIPER PROJECTS ===============*/
+const swiperProjects = new Swiper('.projects__swiper', {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  speed: 600,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+});
 
 
 /*=============== WORK TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const targetSelector = tab.dataset.target,
+          targetContent = document.querySelector(targetSelector)
+
+          //Disable all content and active tabs
+          tabContents.forEach((content) => content.classList.remove('work-active'))
+          tabs.forEach((t) => t.classList.remove('work-active'))
+
+          //Active the tab and corresponding content
+          tab.classList.add('work-active')
+          targetContent.classList.add('work-active')
+  })
+})
 
 
 /*=============== SERVICES ACCORDION ===============*/
+const servicesButtons = document.querySelectorAll('.services__button')
 
+servicesButtons.forEach(button => {
+  //Add your height to services info
+  const heightInfo = document.querySelector('.services__info')
+  heightInfo.style.height = heightInfo.scrollHeight + 'px'
 
-/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
+  button.addEventListener('click', () => {
+    const servicesCards = document.querySelectorAll('.services__card'),
+          currentCard = button.parentNode,
+          currentInfo = currentCard.querySelector('.services__info'),
+          isCardOpen = currentCard.classList.contains('services-open')
+
+    //Close all other services info
+    servicesCards.forEach(card => {
+      card.classList.replace('services-open', 'services-close')
+
+      const info = card.querySelector('.services__info')
+            info.style.height = '0'
+    })
+
+    //Open only if not already open
+    if(!isCardOpen){
+      currentCard.classList.replace('services-close', 'services-open')
+      currentInfo.style.height = currentInfo.scrollHeight + 'px'
+    }
+
+  })
+})
+
+/*=============== SWIPER TESTIMONIALS ===============*/
+const swiperTestimonials = new Swiper('.testimonials__swiper', {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  speed: 600,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+});
 
 
 /*=============== COPY EMAIL IN CONTACT ===============*/
